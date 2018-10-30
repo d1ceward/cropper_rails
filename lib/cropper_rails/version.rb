50d82a345c13225cc80164d6cf619f6302438a1f
Fabien LEFEBVRE <contact@d1ceward.com>
2018-10-30 11:37:18 +0100

    Upgraded cropper version

--- lib/cropper_rails/version.rb
+++ lib/cropper_rails/version.rb
@@ -1,7 +1,7 @@
 module Cropper
   module Rails
-    VERSION = '1.0.1'.freeze
-    CROPPER_VERSION = '1.4.2'.freeze
+    VERSION = '1.0.2'.freeze
+    CROPPER_VERSION = '1.4.3'.freeze
     JQUERY_CROPPER_VERSION = '1.0.0'.freeze
   end
 end
